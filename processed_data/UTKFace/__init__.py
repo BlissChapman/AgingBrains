608339e1a5ff6d57501dc5c3c6a87f396c1dc33e
BlissChapman <bliss.chapman@gmail.com>
2019-03-25 20:36:52 -0500

    Updated processing of UTKFace dataset to process only once

--- processed_data/UTKFace/__init__.py
+++ processed_data/UTKFace/__init__.py
@@ -16,24 +16,26 @@ class Dataset(data.Dataset):
                 on a sample.
         """
         self.root_dir = 'processed_data/UTKFace/images/'
-        self.filenames = os.listdir(self.root_dir)
         self.train = train
         self.transform = transform
+
+        self.data = []
+        for filename in os.listdir(self.root_dir):
+            img_name = os.path.join(self.root_dir, filename)
+            image = io.imread(img_name, as_grey=True).reshape(1, 200, 200)
+            
+            if self.transform:
+                image = self.transform(image).float()
+            else:
+                image = torch.Tensor(image).float()
+
+            match_obj = re.search('/(\d+)_', img_name)
+            age = np.array(int(match_obj.group(1)))
+
+            self.data.append((image, age))
 
     def __len__(self):
         return len(self.filenames)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir,
-                                self.filenames[idx])
-        image = io.imread(img_name, as_grey=True).reshape(200, 200, 1) / 255
-        
-        if self.transform:
-            image = self.transform(image).float()
-        else:
-            image = torch.Tensor(image).float()
-        
-        match_obj = re.search('/(\d+)_', img_name)
-        age = np.array(int(match_obj.group(1)))
-
-        return image, age
+        return data[idx]
