b02d489499dc49f7c75c6fa93a5e846110405aff
Ubuntu <blisschapman@AgingBrains.orrubdf42lbevk5m4sijdvojog.bx.internal.cloudapp.net>
2019-04-11 21:43:01 +0000

    improved utkface dataset

--- data/UTKFace/dataset.py
+++ data/UTKFace/dataset.py
@@ -2,5 +2,5 @@ from torchvision import datasets
 
 class UTKFace(datasets.ImageFolder):
     def __init__(self, transform):
-        super().__init__(root='data/UTKFace/', transform=transform)
+        super().__init__(root='data/UTKFace/processed/', transform=transform)
         
